ca2fedac0543035ff960848e5dec87f4384310f5
batmit <danielmatosfalc@gmail.com>
2025-11-10 19:53:51 +0000

    Co-authored-by: Pedro Henrique <pedroxlbd@gmail.com>

--- formula.h
+++ formula.h
@@ -8,14 +8,15 @@ typedef struct formula Formula;
 
 Formula* criaFormula(int n, int m);
 void destroiFormula(Formula *f);
-void adicionaClausula(Formula *f, int i, int *vetAdd);
+void adicionaClausula(Formula *f);
 void imprimeFormula(Formula *f);
-int solucaoFormula(Formula *f, int n, int *vet); // deve usar recursion
-int verificaCada(Formula *f, int n, int *vet);
-int verificaClausula(Formula *f, int *vet, int n);
+int solucaoFormula(Formula *f, int n, int *vet, int *vetAux); // deve usar recursion
+int verificaCada(Formula *f, int n, int *vet, int *vetAux);
+int verificaClausula(Formula *f, int *vet, int n, int *vetAux);
 void proxVeri(Formula *f, int *vet);
 void limpaAdd(int *vet, int n);
 void imprimirClausulas(Formula *formula);
 
+void limpar_buffer();
 
 #endif
